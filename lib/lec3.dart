9bc96a7ee0b37d504f96cfc89a2779ceb257b5a9
Marwanag <agmarwanag56@gmail.com>
2025-12-03 20:36:16 +0300

    refactor Lec3 widget: update styles, improve layout, and enhance navigation to Screen2

--- lib/lec3.dart
+++ lib/lec3.dart
@@ -1,49 +1,90 @@
 import 'package:flutter/material.dart';
 
- 
 class Lec3 extends StatelessWidget {
-    static const String path = '/lec3';
+  static const String path = '/lec3';
 
   const Lec3({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 100,
-        backgroundColor: Colors.amber,
-        elevation: 20,
-        title: const Text('Scaffold Properties'),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+        backgroundColor: Colors.blue,
+        toolbarHeight: 80,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
         ),
-        actions: [IconButton(icon: const Icon(Icons.info_outline), onPressed: () {})],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Align(alignment: Alignment.centerRight, child: Text('data')),
-          Text('data'),
-          Text('data'),
-          Text('data'),
+        title: Text('scaffold Properties'),
+        // leading: Icon(Icons.arrow_back),
+        actions: [
+          Icon(Icons.arrow_back),
+          Icon(Icons.arrow_back),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.deepPurple),
-              child: Text('Drawer Header', style: TextStyle(color: Colors.white, fontSize: 24)),
-            ),
-            ListTile(leading: const Icon(Icons.home), title: const Text('Home'), onTap: () {}),
-            ListTile(leading: const Icon(Icons.settings), title: const Text('Settings')),
-          ],
+      drawer: HomeDrawerWidget(),
+      endDrawer: HomeDrawerWidget(),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Screen2()),
+            );
+          },
+          child: Text('go to screen2'),
         ),
       ),
+    );
+  }
+}
+
+class HomeDrawerWidget extends StatelessWidget {
+  const HomeDrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 0,
+      width: 300,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            color: Colors.redAccent,
+            child: Text('hello '),
+          ),
+          ListTile(
+            title: Text('title'),
+            subtitle: Text('subtitle'),
+            leading: Icon(Icons.arrow_back),
+            trailing: Icon(Icons.ac_unit_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('screen2')),
+      body: Center(child: Text('hello user')),
     );
   }
 }
